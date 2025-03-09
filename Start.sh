fdb543c2b6f28aea0a45fa9d0a1dac20b8724f2d
arthur <lion811004@gmail.com>
2025-03-08 22:16:07 -0500

    add test

--- Start.sh
+++ Start.sh
@@ -4,3 +4,9 @@ echo "Sleep 5 sec"
 sleep 5
 
 docker-compose exec ollama-app /bin/sh -c "ollama pull llama3:8b-instruct-q8_0"
+
+echo "Sleep 5 sec"
+sleep 5
+
+echo "Health test"
+curl http://localhost:11434/api/generate -d '{"model": "llama3:8b-instruct-q8_0", "prompt": "hello"}'
